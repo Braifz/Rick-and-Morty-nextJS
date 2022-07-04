566c3e3f17aa267dfc944908f1dae36909b0f433
Braifz <braian94lp@gmail.com>
2022-07-03 22:39:10 -0300

    create component footer and styles

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import styles from "../styles/footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <a href="https://www.github.com/braifz">@braifz</a>
+    </footer>
+  );
+};
+
+export default Footer;
