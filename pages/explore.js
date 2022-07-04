b992ba87905a121a886a861283589f3bfc1f95f0
Braifz <braian94lp@gmail.com>
2022-07-03 22:37:10 -0300

    create page explore

---
+++ pages/explore.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+import styles from "../styles/explore.module.css";
+
+const Hello = () => {
+  return (
+    <div className={styles.container}>
+      <Layout>
+        <h1>Aca se van a buscar los personajes</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Hello;
