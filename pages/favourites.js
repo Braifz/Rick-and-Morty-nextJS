a302be7ae807c586da1aea3b0666b0f894c07044
Braifz <braian94lp@gmail.com>
2022-07-03 22:37:30 -0300

    create page favorites

---
+++ pages/favourites.js
@@ -0,0 +1,14 @@
+import Layout from "../components/Layout";
+import styles from "../styles/favorites.module.css";
+
+const Favourites = () => {
+  return (
+    <div className={styles.container}>
+      <Layout>
+        <h1>favourites</h1>
+      </Layout>
+    </div>
+  );
+};
+
+export default Favourites;
