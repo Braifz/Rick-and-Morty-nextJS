a1177b86ad79a958911d2f2b198798da342e44cd
Braifz <braian94lp@gmail.com>
2022-07-05 10:51:38 -0300

    add code Character component

--- components/Character.js
+++ components/Character.js
@@ -1,13 +1,21 @@
 import Image from "next/image";
+import styles from "../styles/character.module.css";
 
 const Character = ({ data }) => {
   return (
-    <div>
+    <div className={styles.characterCard}>
       <h1>{data.name}</h1>
-      <p>{data.status}</p>
-      <p>{data.species}</p>
-      <p>{data.gender}</p>
-      <Image width={300} height={100} src={data.image} alt={data.name} />
+      <div className={styles.dataContainer}>
+        <p>status: {data.status}</p>
+        <p>specie: {data.species}</p>
+      </div>
+      <Image
+        className={styles.image}
+        src={data.image}
+        width={300}
+        height={300}
+        alt={data.name}
+      />
     </div>
   );
 };
