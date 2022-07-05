0a661810c3ba3d304271b89d763e40141891bb88
Braifz <braian94lp@gmail.com>
2022-07-05 10:52:56 -0300

    add some code Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from "../styles/layout.module.css";
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
-      {children}
+      <div className={styles.childrenContainer}>{children}</div>
       <Footer />
     </>
   );
