4ad75a56eac3b3d15e50016c4a705cb68219ed89
Braifz <braian94lp@gmail.com>
2022-07-05 10:52:16 -0300

    add map to characters

---
+++ components/Characters.js
@@ -0,0 +1,14 @@
+import Character from "./Character";
+import styles from "../styles/characters.module.css";
+
+const Characters = ({ data }) => {
+  return (
+    <div className={styles.container}>
+      {data.results.map((e) => (
+        <Character key={e.id} data={e} />
+      ))}
+    </div>
+  );
+};
+
+export default Characters;
