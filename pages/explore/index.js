930757f5cf87231d31efeda1bcbe7cf521ca71ce
Braifz <braian94lp@gmail.com>
2022-07-09 20:45:31 -0300

    add button for change page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -1,24 +1,36 @@
+import { useState } from "react";
 import Layout from "../../components/Layout";
-import Character from "../../components/Character";
 import Characters from "../../components/Characters";
 import styles from "../../styles/explore.module.css";
 
-const Hello = ({ data }) => {
+const Explore = ({ data }) => {
+  const [page, setPage] = useState(1);
+
+  const handleClick = (numPag) => {
+    if (page === 1) {
+      setPage(page);
+    }
+    numPag ? setPage(page + 1) : setPage(page - 1);
+  };
   console.log(data);
   return (
     <Layout>
       <div className={styles.container}>
         <h1>Aca se van a buscar los personajes</h1>
         <Characters data={data} />
+        <button onClick={handleClick(true)}>Prev</button>
+        <button onClick={handleClick(false)}>Next</button>
       </div>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
-  const res = await fetch("https://rickandmortyapi.com/api/character");
+  const res = await fetch(
+    `https://rickandmortyapi.com/api/character?page=${1}`
+  );
   const data = await res.json();
   return { props: { data } };
 }
 
-export default Hello;
+export default Explore;
