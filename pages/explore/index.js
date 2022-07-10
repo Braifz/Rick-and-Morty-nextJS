c0ae7787b05d95ef7eca4c815a0de5188e5929e8
Braifz <braian94lp@gmail.com>
2022-07-09 23:03:52 -0300

    add seachCharacter to explore page

--- pages/explore/index.js
+++ pages/explore/index.js
@@ -1,33 +1,61 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import Characters from "../../components/Characters";
 import styles from "../../styles/explore.module.css";
+import SearchCharacter from "../../components/SeachCharacter";
 
 const Explore = ({ data }) => {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(5);
 
-  const handleClick = (numPag) => {
+  useEffect(() => {
+    console.log(page);
+  }, [page]);
+
+  const handleClickPrev = () => {
     if (page === 1) {
       setPage(page);
+    } else {
+      setPage(page - 1);
     }
-    numPag ? setPage(page + 1) : setPage(page - 1);
+  };
+
+  const handleClickNext = () => {
+    setPage(page + 1);
   };
   console.log(data);
   return (
     <Layout>
       <div className={styles.container}>
-        <h1>Aca se van a buscar los personajes</h1>
+        <SearchCharacter />
         <Characters data={data} />
-        <button onClick={handleClick(true)}>Prev</button>
-        <button onClick={handleClick(false)}>Next</button>
+        <div>
+          <button onClick={handleClickPrev}>Prev</button>
+          <button onClick={handleClickNext}>Next</button>
+        </div>
       </div>
+      <style jsx>
+        {`
+          button {
+            margin: 32px;
+            padding: 16px;
+            transition: all 0.3s;
+            border-radius: 16px;
+            background-color: #fff;
+            color: var(--primary-bg-color);
+          }
+          button:hover {
+            box-shadow: 1px 1px 5px #fff;
+            transform: scale(1.5);
+          }
+        `}
+      </style>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
   const res = await fetch(
-    `https://rickandmortyapi.com/api/character?page=${1}`
+    `https://rickandmortyapi.com/api/character?page=${9}`
   );
   const data = await res.json();
   return { props: { data } };
