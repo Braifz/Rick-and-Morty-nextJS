c6edff37f7d56d49e11364c9f1852e966f71a7c4
Braifz <braian94lp@gmail.com>
2022-07-26 12:11:59 -0300

    add map favorites characters

--- pages/favourites.js
+++ pages/favourites.js
@@ -1,13 +1,37 @@
+import { useContext, useState, useEffect } from "react";
+
 import Layout from "../components/Layout";
 import styles from "../styles/favorites.module.css";
+import { FavoriteContext } from "../contexts/favorite";
+import Characters from "../components/Characters";
 
 const Favourites = () => {
+  const [data, setData] = useState([]);
+
+  const { state } = useContext(FavoriteContext);
+
+  useEffect(() => {
+    setData(state);
+  }, [state]);
+
+  const handleClick = () => {
+    console.log("hello", state, data);
+  };
+
   return (
-    <div className={styles.container}>
-      <Layout>
-        <h1>favourites</h1>
-      </Layout>
-    </div>
+    <Layout>
+      <div className={styles.container}>
+        <h1 className={styles.text}>favourites</h1>
+        <button className={styles.text} onClick={handleClick}>
+          getFavorite
+        </button>
+        {data.favorites?.length > 0 ? (
+          <Characters data={data.favorites} />
+        ) : (
+          <p>No hay favoritos</p>
+        )}
+      </div>
+    </Layout>
   );
 };
 
