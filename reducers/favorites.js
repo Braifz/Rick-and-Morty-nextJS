5a80b486be4c0d047723213768acee36eee3cba1
Braifz <braian94lp@gmail.com>
2022-07-26 12:13:00 -0300

    change payload reducers

--- reducers/favorites.js
+++ reducers/favorites.js
@@ -1,4 +1,8 @@
-import { ADD_FAVORITES, DELETE_FAVORITES } from "../actions/favorite";
+import {
+  ADD_FAVORITES,
+  DELETE_FAVORITES,
+  GET_FAVORITES,
+} from "../actions/favorite";
 
 export const initialState = {
   favorites: [],
@@ -8,11 +12,15 @@ export const favoriteReducer = (state, action) => {
   switch (action.type) {
     case ADD_FAVORITES: {
       return {
-        favorites: [...state.favorites, action.payload],
+        favorites: [...state.favorites, action.payload.data],
       };
     }
     case DELETE_FAVORITES: {
       return {};
+    }
+    case GET_FAVORITES: {
+      console.log("get_favorite", state.favorites);
+      return state.favorites;
     }
     default:
       return state;
