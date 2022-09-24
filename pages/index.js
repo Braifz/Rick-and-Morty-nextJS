c1caea5131365181fd774c80d18f1a507427854a
Braifz <braian94lp@gmail.com>
2022-09-24 15:07:33 -0300

    add page

--- pages/index.js
+++ pages/index.js
@@ -9,26 +9,10 @@ import styles from "../styles/index.module.css";
 import HomeContent from "../components/HomeContent";
 import { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import Model from "../components/Tree";
+import { PickleRick } from "../components/PickleRick";
 
-const Box = lazy(() => import("../components/Box"));
+// const Box = lazy(() => import("../components/Box"));
 
-// function Model({ ...props }) {
-//   const group = useRef();
-//   const { nodes, materials } = useGLTF("/scene.gltf");
-//   return (
-//     <group ref={group} {...props} dispose={null}>
-//       <group rotation={[-Math.PI / 2, 0, 0]}>
-//         <mesh
-//           geometry={nodes.Object_2.geometry}
-//           material={materials.material_0}
-//         />
-//       </group>
-//     </group>
-//   );
-// }
-
-// useGLTF.preload("/scene.gltf");
 const KeyLight = ({ brightness, color }) => {
   return (
     <rectAreaLight
@@ -70,15 +54,12 @@ export default function Home() {
             <ThreeScene>
               <Suspense fallback={null}>
                 {/* <Box /> */}
-                <Model />
+                {/* <PickleRick /> */}
                 <ambientLight />
               </Suspense>
               <Camera />
               <KeyLight brightness={5.6} color={"#fff"} />
               <FillLight brightness={2.6} color={"#fff"} />
-              {/* <Suspense fallback={null}>
-                <Model />
-              </Suspense> */}
               <OrbitControls />
             </ThreeScene>
           </div>
