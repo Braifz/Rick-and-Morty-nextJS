86768bfbf73e6323a2c726bf40f26e9902e0aa3e
Braifz <braian94lp@gmail.com>
2022-09-25 13:49:07 -0300

    change params camera

--- components/Camera.js
+++ components/Camera.js
@@ -1,7 +1,7 @@
 import { PerspectiveCamera } from "@react-three/drei";
 
 const Camera = () => {
-  return <PerspectiveCamera makeDefault position={[5, 1, 1]} fov={25} />;
+  return <PerspectiveCamera makeDefault position={[400, 600, 1]} fov={25} />;
 };
 
 export default Camera;
