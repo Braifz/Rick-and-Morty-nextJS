4e8c2992947cfdcad255deb8d3404f5ac7bd2d65
Braifz <braian94lp@gmail.com>
2022-09-24 22:25:21 -0300

    add motion.div Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import styles from "../styles/layout.module.css";
+import { motion } from "framer-motion";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.3 }}
+    >
       <Header />
       <div className={styles.childrenContainer}>{children}</div>
       <Footer />
-    </>
+    </motion.div>
   );
 };
 
