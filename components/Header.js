f2e563c04e0911f19f01847c202eaaa50ee74961
Braifz <braian94lp@gmail.com>
2022-10-04 13:12:32 -0300

    add logo rick and morty and change flex

--- components/Header.js
+++ components/Header.js
@@ -2,34 +2,73 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState } from "react";
 import styles from "../styles/header.module.css";
-import NavImg from "../public/nav-ico.png";
 import favImg from "../public/images/favorite-star.svg";
+import rickAndMortyLogo from "../public/rickandmorty.svg";
 
 const Header = () => {
-  const [isActive, setIsActive] = useState(false);
-
-  const toggleNav = () => {
-    setIsActive(!isActive);
-  };
-  console.log(isActive);
+  const [isOpen, setIsOpen] = useState(false);
+  const openMenu = () => setIsOpen(!isOpen);
 
   return (
-    <header className={styles.container}>
-      <nav className={styles.nav}>
-        <Link className={styles.element} href="/">
-          <a className={styles.element}>Home</a>
-        </Link>
-        <Link className={styles.element} href="/explore">
-          <a className={styles.element}>Explore</a>
-        </Link>
-        <Link className={styles.element} href="/favourites">
-          {/* <Image src={favImg} alt="favourite icon" width={30} height={30} /> */}
-          <a className={styles.element}> Favorite</a>
-        </Link>
+    <header className={styles.header}>
+      <Link href="/">
+        <a>
+          <Image
+            src={rickAndMortyLogo}
+            height={150}
+            width={250}
+            alt="rick and morty logo"
+          />
+        </a>
+      </Link>
+      <nav className={styles.navbar}>
+        <ul
+          className={
+            isOpen === false
+              ? styles.navmenu
+              : styles.navmenu + " " + styles.active
+          }
+        >
+          <li className={styles.navitem}>
+            <Link href="/explore">
+              <a
+                className={
+                  isOpen === false
+                    ? styles.navlink
+                    : styles.navlink + " " + styles.active
+                }
+              >
+                Explore
+              </a>
+            </Link>
+          </li>
+          <li className={styles.navitem}>
+            <Link href="/favourites">
+              <a
+                className={
+                  isOpen === false
+                    ? styles.navlink
+                    : styles.navlink + " " + styles.active
+                }
+              >
+                Favorite
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <button
+          onClick={openMenu}
+          className={
+            isOpen === false
+              ? styles.hamburger
+              : styles.hamburger + " " + styles.active
+          }
+        >
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </button>
       </nav>
-      <button onClick={toggleNav} className={styles.navIco}>
-        <Image src={NavImg} alt="nav-ico" width={50} height={50} />
-      </button>
     </header>
   );
 };
