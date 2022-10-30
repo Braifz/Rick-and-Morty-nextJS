b9c4f02638e0a71bcc7db22d0349e9f328540643
Braifz <braian94lp@gmail.com>
2022-10-30 19:53:16 -0300

    create signupform

--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,6 @@
-import { Form, Formik } from "formik";
+import { useState } from "react";
+import { Form, Formik, Field } from "formik";
+import Link from "next/link";
 import styles from "../styles/signup.module.css";
 
 const signup = () => {
@@ -10,26 +12,58 @@ const signup = () => {
     password: "",
   };
 
-  const onSubmit = (values) => {
+  const onSubmit = (values, { resetForm }) => {
     console.log(values);
+    resetForm({ values: "" });
   };
 
   return (
     <div className={styles.container}>
-      <Formik>
-        <form className={styles.form}>
+      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+        <Form className={styles.form}>
+          <h1 className={styles.title}>Sign Up</h1>
           <label>firstName</label>
-          <input />
+          <Field
+            className={styles.input}
+            id="firstName"
+            name="firstName"
+            type="text"
+          />
           <label>lastname</label>
-          <input />
+          <Field
+            className={styles.input}
+            id="lastName"
+            name="lastName"
+            type="text"
+          />
           <label>email</label>
-          <input />
+          <Field
+            className={styles.input}
+            id="email"
+            name="email"
+            type="email"
+          />
           <label>username</label>
-          <input />
+          <Field
+            className={styles.input}
+            id="username"
+            name="username"
+            type="text"
+          />
           <label>password</label>
-          <input />
-          <button type="submit">Submit</button>
-        </form>
+          <Field
+            className={styles.input}
+            id="password"
+            name="password"
+            type="password"
+          />
+          <button className={styles.btn} type="submit">
+            Submit
+          </button>
+          <div className={styles.signin}>
+            <Link href="/signin">Are you already registered? Sign In</Link>
+          </div>
+        </Form>
       </Formik>
     </div>
   );
